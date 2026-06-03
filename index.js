200e95f25fc92535ac94da44c79e4a26803c64af
Antonio Castellon <antonio@castellon.ch>
2026-06-03 10:19:53 +0200

    Update 2026: add missing package.json, .gitignore, LICENSE for consistency, update README

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// Placeholder for WS DNS server (ad-hoc discovery)
+// Implement REST endpoints for /v1/set/:service , /v1/get/:service , /v1/ping
+
+module.exports = function(setup) {
+  // TODO: implement DNS registry server
+  return { start: () => console.log('ws-dns placeholder') };
+};
